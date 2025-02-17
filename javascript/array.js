e5836ece886f1f38cb21daa01b51356dd962bca2
Abhay Sharma <abhaysharmafalakata@gmail.com>
2025-02-17 22:21:48 +0530

    day 4

--- javascript/array.js
+++ javascript/array.js
@@ -9,3 +9,31 @@ console.log(typeof Number(val));
 
 console.log(typeof arr[0])
 console.log(typeof arr[1])
+
+
+var arr2 = ["buggati", "lamborgini", "ferrari", "audi"];
+//or
+var arr2 = new Array("buggati", "lamborgini", "ferrari", "audi");
+console.log(arr2[0]);
+
+arr2[arr2.length] = "bmw";
+arr2.push("mercedes");
+var arr3 = arr2;
+var popped = arr2.pop();
+console.log(arr2[4]);//undefined
+console.log("popped element: " + popped);
+
+console.log(arr2, arr3);
+
+var arr4=arr2.concat(arr3);
+console.log(arr4);
+let shif = arr4.shift();
+arr4.unshift("toyota", "honda");
+console.log(shif);
+console.log(arr4);
+
+let arr5 = arr4.slice(1, 4);
+console.log("arr5: "+arr5);
+
+let includes = arr4.includes("bmw");//boolean
+console.log(includes+typeof includes);
---
+++ javascript/ques.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3];
+arr.push(4);
+arr.pop();
+console.log(arr);
+
+arr.reverse();
+console.log(arr);
+
+console.log(arr.indexOf(0));
