3e3461beb27db3537fffd5603cda4dad7cc04890
Abhay Sharma <abhaysharmafalakata@gmail.com>
2025-03-01 16:12:55 +0530

    oops

--- javascript/oops.js
+++ javascript/oops.js
@@ -8,4 +8,49 @@ let car = {
   },
 };
 console.log(car.start());
-// What is the output?
+
+Array.prototype.start = function () {
+  return `${this[0]} started`;
+};
+
+let myArray = [1, 1, 2, 3];
+console.log(myArray.start());
+
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  start() {
+    return `${this.make} is a car from ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  drive() {
+    return `${this.make} ${this.model} is driving`;
+  }
+}
+
+let vehicle = new Vehicle("Toyota", "Corolla", 2019);
+console.log(vehicle.year);
+console.log(vehicle.start());
+
+class BankAcc {
+  #balance = 100;
+
+  deposit(amount) {
+    this.#balance += amount;
+    return this.#balance;
+  }
+  getBalance(){
+    return this.#balance;
+  }
+}
+
+let acc = new BankAcc();
+acc.deposit(100);
+console.log(acc.getBalance());
+// console.log(acc.#balance); // Error
